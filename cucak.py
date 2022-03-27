dd26d67d2c528322ae09400d71db500c2546e201
nvrfbck <suzanna2002@gmail.com>
2022-03-27 08:37:19 +0400

    list02

--- cucak.py
+++ cucak.py
@@ -1,2 +1,19 @@
 print("cucak")
 
+#sarking cucak dartak u guzenq erkarutyun@
+lst = []
+ellen = input("сколько элементов будет в списке? : ")
+#amen andami hamar viklov uzum enq int
+for i in range(0, int(ellen)):
+    print("элемент номер", i, ": ", end="")
+    el = input()
+    # kpcnum enq
+    lst.append(el)
+# listi print@!
+print(lst)
+# ete ka grum enq vorerordn e
+n = input("информацию о каком элементе получить?: ")
+if n in lst:
+    print("такой элемент есть. это элемент номер", lst.index(n))
+else:
+    print("такого элемента нет. ")
